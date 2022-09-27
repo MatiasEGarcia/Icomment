28837c10a1ccc0e2e60572d2c8f7ffd9fe78c966
Matias.E.Garcia <matiasegarcia22@gmail.com>
2022-09-27 11:32:25 -0300

    first commit

---
+++ src/main/java/com/icomment/icomment/IcommentApplication.java
@@ -0,0 +1,13 @@
+package com.icomment.icomment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcommentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icomment/icomment/IcommentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icomment.icomment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcommentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
