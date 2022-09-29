ad1e98315a691acaabfbe5bb0df644eb45be8d31
Matias.E.Garcia <matiasegarcia22@gmail.com>
2022-09-29 14:49:56 -0300

    Rol dao , User dao

---
+++ src/main/java/com/icomment/icomment/dao/RolDao.java
@@ -0,0 +1,9 @@
+package com.icomment.icomment.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.icomment.icomment.domain.Rol;
+
+public interface RolDao extends JpaRepository<Rol, Long>{
+
+}
