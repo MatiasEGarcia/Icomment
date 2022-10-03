11874658657f792481de8e39b4523e148906c496
Matias.E.Garcia <matiasegarcia22@gmail.com>
2022-10-03 09:58:00 -0300

    attemptAuthentication,successfulAuthentication

--- src/main/java/com/icomment/icomment/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/icomment/icomment/filter/CustomAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package com.icomment.icomment.filter;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -11,7 +13,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.icomment.icomment.service.UserDetailsImpl;
 
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 	
@@ -32,7 +39,23 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 	@Override
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
 			Authentication authResult) throws IOException, ServletException {
-		super.successfulAuthentication(request, response, chain, authResult);
+		UserDetailsImpl user = (UserDetailsImpl) authResult.getPrincipal();
+		Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+		String access_token = JWT.create()
+				.withSubject(user.getUsername())
+				.withExpiresAt(new Date(System.currentTimeMillis()+10*60*1000))
+				.withIssuer(request.getRequestURL().toString())
+				.withClaim("roles",user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+				.sign(algorithm);
+	
+		String refresh_token= JWT.create()
+				.withSubject(user.getUsername())
+				.withExpiresAt(new Date(System.currentTimeMillis()+30*60*1000))
+				.withIssuer(request.getRequestURL().toString())
+				.sign(algorithm);
+	
+		response.addHeader("access_token", access_token);
+		response.addHeader("refresh_token", refresh_token);
 	}
 
 
