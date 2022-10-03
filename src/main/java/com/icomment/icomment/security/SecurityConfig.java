96588071f943d3e92ea8d9f371931993c92d3735
Matias.E.Garcia <matiasegarcia22@gmail.com>
2022-10-03 10:20:34 -0300

    authenticationManager bean deleted

--- src/main/java/com/icomment/icomment/security/SecurityConfig.java
+++ src/main/java/com/icomment/icomment/security/SecurityConfig.java
@@ -3,9 +3,7 @@ package com.icomment.icomment.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -39,10 +37,5 @@ public class SecurityConfig {
 		http.apply(MyCustomDsl.customDsl());
 		return http.build();
 	}
-	
-	@Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
 
 }
