bd03afc09bf55b5d50d544358474a67d7a947a79
Matias.E.Garcia <matiasegarcia22@gmail.com>
2022-10-04 21:08:59 -0300

    JwtUtils getRefreshToken and getUsernameFromToken

--- src/main/java/com/icomment/icomment/security/jwt/JwtUtils.java
+++ src/main/java/com/icomment/icomment/security/jwt/JwtUtils.java
@@ -2,21 +2,36 @@ package com.icomment.icomment.security.jwt;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.icomment.icomment.domain.Rol;
 import com.icomment.icomment.service.UserDetailsImpl;
 
 public class JwtUtils {
+	private String authorizationHeader;
+	private final String startsWith = "Bearer ";
 	private final String algorithmKey = "secret";
 	private final Algorithm algorithm = Algorithm.HMAC256(algorithmKey.getBytes());
 	
 	public JwtUtils() {
 		
+	}
+	
+	public JwtUtils(String authorizationHeader) throws Exception { 
+		this.authorizationHeader = authorizationHeader;
+		if(authorizationHeader == null){
+			throw new Exception("Refresh token missing");
+		}else if (!authorizationHeader.startsWith(startsWith)) {
+			throw new Exception("Refresh token missing");
+		}
 	}
 	
 	public Map<String,String> getSuccessfulAuthTokens(UserDetailsImpl user, String issuer){
@@ -39,5 +54,29 @@ public class JwtUtils {
 		tokens.put("refreshToken",refreshToken);
 		return tokens;
 	}
+	
+	public Map<String,String> getRefreshToken(String username,List<Rol> roles,String issuer){
+		Map<String, String> tokens = new HashMap<>();
+		String refresToken = authorizationHeader.substring(startsWith.length());
+		
+		String accessToken = JWT.create()
+				.withSubject(username)
+				.withExpiresAt(new Date(System.currentTimeMillis()+10*60*1000)) //10min
+				.withIssuer(issuer)
+				.withClaim("roles",roles.stream().map(rol-> rol.getName().toString()).collect(Collectors.toList()))
+				.sign(algorithm);
+	
+		tokens.put("accessToken",accessToken);
+		tokens.put("refreshToken",refresToken);
+		return tokens;
+	}
+	
+	public String getUsernameFromToken() {
+		String token = authorizationHeader.substring(startsWith.length());
+		JWTVerifier verifier = JWT.require(algorithm).build();
+		DecodedJWT decodedJWT = verifier.verify(token);
+		return decodedJWT.getSubject();
+	}
+	
 	
 }
